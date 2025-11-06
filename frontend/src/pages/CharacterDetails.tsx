3b902c639a2adf8f653458ffe903454b1bc6761d
ewertonlx <ewerton123ofc@gmail.com>
2025-11-06 16:53:20 -0300

    feat: hability search in input

--- frontend/src/pages/CharacterDetails.tsx
+++ frontend/src/pages/CharacterDetails.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { NavBar } from '../components/NavBar';
-import { translateSpecies, translateStatus } from '../functions/translateText';
+import { tranlateGender, translateSpecies, translateStatus } from '../functions/translateText';
 import { Location } from '../components/Location';
 import { getIdFromUrl } from '../functions/getIdFromUrl';
+import { CardCharacter } from '../components/CardCharacter';
 
 interface Character {
   id: number;
@@ -18,6 +19,7 @@ interface Character {
 }
 
 export function CharacterDetails() {
+  const [searchTerm, setSearchTerm] = useState("");
   const { id } = useParams<{ id: string }>();
   const [character, setCharacter] = useState<Character | null>(null);
   const statusInfo = translateStatus(character?.status || "");
@@ -33,31 +35,40 @@ export function CharacterDetails() {
   if (!character) 
   return (
     <main>
-      <NavBar />
+      <NavBar onSearch={setSearchTerm}/>
       <div className="flex items-center justify-center h-screen">
         <p className="text-4xl font-bold">Carregando...</p>
       </div>
     </main>
   );
 
+  if(searchTerm){
+    return (
+      <main>
+        <NavBar onSearch={setSearchTerm}/>
+        <CardCharacter searchTerm={searchTerm} />
+      </main>
+    )
+  }
+
   return (
     <main>
-      <NavBar />
-      <div className="flex m-10 flex-row gap-2 items-start">
-        <img src={character.image} alt={character.name} className="border p-4 shadow-md rounded-lg w-64 mb-4" />
+      <NavBar onSearch={setSearchTerm}/>
+      <div className="flex m-10 flex-row gap-2 items-start max-sm:flex-col max-sm:items-center max-sm:m-2">
+        <img src={character.image} alt={character.name} className="border p-4 shadow-md rounded-lg w-64 mb-4 max-sm:w-42" />
         <div className="ml-4">
           <h2 className="text-3xl font-bold mb-2">{character.name}</h2>
           <p>Status: <span className={statusInfo.color}>{statusInfo.text}</span></p>
           <p>Espécie: <span className={speciesInfo.color}>{speciesInfo.text}</span></p>
-          <p>Gênero: {character.gender}</p>
+          <p>Gênero: <span className={tranlateGender(character.gender).color}>{tranlateGender(character.gender).text}</span></p>
           <p>Origem: {character.origin.name}</p>
         </div>
       </div>
-      <div className="m-10">
+      <div className="m-10 max-sm:m-2">
         <h3 className="text-2xl font-bold mb-2">Localização</h3>
         <Location locationId={getIdFromUrl(character.location.url)!} />
       </div>
-      <div className="m-10">
+      <div className="m-10 max-sm:m-2">
         <h3 className="text-2xl font-bold mb-2">Episódios em que esse personagem esteve presente</h3>
         <ul className="flex flex-wrap gap-4 list-none p-2 m-2">
           {character.episode?.map((epUrl) => {
--- frontend/src/pages/EpisodeDetails.tsx
+++ frontend/src/pages/EpisodeDetails.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { NavBar } from '../components/NavBar';
-//import { translateSpecies, translateStatus } from '../functions/translateText';
+import { CardCharacter } from '../components/CardCharacter';
+import { Episode } from '../components/Episode';
 
 interface Episode {
   name: string;
@@ -11,10 +12,9 @@ interface Episode {
 }
 
 export function EpisodeDetails() {
+  const [searchTerm, setSearchTerm] = useState("");
   const { id } = useParams<{ id: string }>();
   const [episode, setEpisode] = useState<Episode | null>(null);
-  // const statusInfo = translateStatus(episode?.status || "");
-  // const speciesInfo = translateSpecies(episode?.species || "");
 
   useEffect(() => {
     fetch(`http://localhost:8080/api/episodes/${id}`)
@@ -26,24 +26,26 @@ export function EpisodeDetails() {
   if (!episode) 
   return (
     <main>
-      <NavBar />
+      <NavBar onSearch={setSearchTerm}/>
       <div className="flex items-center justify-center h-screen">
         <p className="text-4xl font-bold">Carregando...</p>
       </div>
     </main>
   );
 
+  if(searchTerm){
+    return (
+      <main>
+        <NavBar onSearch={setSearchTerm}/>
+        <CardCharacter searchTerm={searchTerm} />
+      </main>
+    )
+  }
+
   return (
     <main>
-      <NavBar />
-      <div className="flex m-10 flex-row gap-2 items-start">
-        <div className="ml-4">
-          <h2 className="text-3xl font-bold mb-2">Episódio - {episode.name}</h2>
-          <p>Data de lançamento: <span className="text-green-700">{episode.air_date}</span></p>
-          <p>Identificador: <span className="text-red-600">{episode.episode}</span></p>
-          <p>Personagens: <span className="text-gray-600">{episode.characters.length}</span></p>
-        </div>
-      </div>
+      <NavBar onSearch={setSearchTerm} />
+      <Episode episodeId={id || ""} />
     </main>
   );
 }
